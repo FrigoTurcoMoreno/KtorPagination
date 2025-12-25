cc7f1a4de1d9ea311dba6f36affb00434c22f4d5
FrigoTurcoMoreno <frigoturcomoreno@gmail.com>
2025-12-25 20:33:00 +0100

    add configuration to push on maven central

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.kotlin.jvm)
     alias(libs.plugins.kotlin.plugin.serialization)
+    id("com.vanniktech.maven.publish") version "0.35.0"
 }
 
 group = "dev.mft"
@@ -10,6 +11,44 @@ val artifact = "ktor_pagination"
 repositories {
     mavenCentral()
 }
+
+mavenPublishing {
+    publishToMavenCentral()
+
+    signAllPublications()
+
+    coordinates("$group.$artifact", "ktor_pagination", version.toString())
+
+    pom {
+        name.set("Ktor Pagination")
+        description.set("Kotlin pagination extension library for Exposed ORM with built-in support for all entity types")
+        inceptionYear.set("2025")
+        url.set("https://github.com/FrigoTurcoMoreno/KtorPagination")
+
+        licenses {
+            license {
+                name.set("MIT License")
+                url.set("https://opensource.org/licenses/MIT")
+                distribution.set("repo")
+            }
+        }
+
+        developers {
+            developer {
+                id.set("FrigoTurcoMoreno")
+                name.set("Moreno Frigo Turco")
+                url.set("https://github.com/FrigoTurcoMoreno")
+            }
+        }
+
+        scm {
+            url.set("https://github.com/FrigoTurcoMoreno/KtorPagination")
+            connection.set("scm:git:git://github.com/FrigoTurcoMoreno/KtorPagination.git")
+            developerConnection.set("scm:git:ssh://git@github.com/FrigoTurcoMoreno/KtorPagination.git")
+        }
+    }
+}
+
 
 dependencies {
     compileOnly(libs.ktor.serialization.kotlinx.json)
